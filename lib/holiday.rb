f8207f5ac9120d4a1835a6e1cd986a62eb94c051
Brad Hambrice <hambrice@uab.edu>
2018-01-06 20:40:32 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,7 +54,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season.capitalize}:"
     holidays.each do |holiday, supply|
-      holiday = holiday.to_s.capitalize
+      holiday = holiday.to_s.capitalize.sub("_", " ")
       puts "  #{holiday}: #{supply.join(", ")}"
   end
   end
