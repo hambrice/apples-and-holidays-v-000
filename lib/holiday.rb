cb2e6e8f3ebe9293734bf9d1e75983299c388bb8
Brad Hambrice <hambrice@uab.edu>
2018-01-06 20:50:03 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
       holidaze.each do |name|
         name.capitalize!
       end
-      holidaze.join
+      holidaze.join!
       puts "  #{holidaze}: #{supply.join(", ")}"
   end
   end
